a45e60e05fd790acae4f06fb75720ae11b630666
akashkiragi <akashkiragi5@gmail.com>
2024-03-14 23:57:13 +0530

    SpringBoot japa app

---
+++ src/main/java/com/kiragi/practice/CourseApiDbApplication.java
@@ -0,0 +1,13 @@
+package com.kiragi.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiDbApplication.class, args);
+	}
+
+}
